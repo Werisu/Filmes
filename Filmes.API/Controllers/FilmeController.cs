b79ff10369e9620aa1e96b9a8aa0cac5bf725f8a
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2023-02-08 16:24:22 -0300

    Adicionar arquivos de projeto.

---
+++ Filmes.API/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using Filmes.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Filmes.API.Controllers;
+[ApiController]
+[Route("Filme")]
+public class FilmeController : ControllerBase
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void Post([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+    }
+}
